b4c9a8e04e12476876b5cfe66a37e788169b8d7f
Bruno José Marques Da Silva <78493454+Lancelotti-beta@users.noreply.github.com>
2024-10-04 16:21:06 -0300

    . .

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ async function mostraInformacaos(){
     await fetch("./info.json", "GET")
       .then((resposta) => resposta.json)
       .then((dados) => {
-        document.querySelector('data-info="eu"').innerHTML =
-        `
+        document.querySelector('[data-info="eu"]').innerHTML = `
           ${dados.name}
           <span class="texto--block">${dados.age} anos</span>
           <span class="texto--block titulo--secundario">ONEhellper</span>
-        `
-        dados.trabalhos.map(({}) => {
+        `;
+        document.querySelector('[data-info="trabalhos"]').innerHTML = `
+          <ul>
+            ${dados.trabalhos.map(({empresa, cargo, dataInicio, dataFinal}) => {
+              ``
 
-        })
+            })}
+          </ul>
+        `;
       });
       
 
