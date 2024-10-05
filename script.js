d2df74a5bb8249f548c6a6bf3669c33f538d023b
Bruno José Marques Da Silva <78493454+Lancelotti-beta@users.noreply.github.com>
2024-10-05 20:20:16 +0000

    . . .

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-console.log(analytics.database());
+console.log(app);
 
 const terminoDeContrato = (value) => null ? " - . ." : " - " + value;
 
