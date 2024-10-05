204cff06bd904740377753002f77e93e10a3d96a
Bruno José Marques Da Silva <78493454+Lancelotti-beta@users.noreply.github.com>
2024-10-05 01:51:15 +0000

    refatorando

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 (() => {
 
-document.querySelector('h1').innerHTML = "";
+function informacaoPessoal(dados) {
+  return ` 
+    ${dados.name}
+    <span class="texto--block">${dados.age} anos</span>
+    <span class="texto--block titulo--secundario">ONEhellper</span>
+  `
+}
+
+function trabalhosRegistrados(dados) {
+  dados.trabalhos.map(({ empresa, cargo, dataInicio, dataFinal }) => {
+    return `
+      <li class="texto">
+        <span class="titulo--secundario">
+          ${empresa} ( ${dataInicio} ${null ? ' - . . .' : ' - ' dataFinal} )
+        </span > - ${cargo}
+      </li>
+    `
+  })
+}
+
 
 async function mostraInformacaos(){
-    await fetch("./info.json", { method : "GET" })
-      .then((resposta) => resposta.json())
-      .then((dados) => {
-        document.querySelector('[data-info="eu"]').innerHTML = `
-          ${dados.name}
-          <span class="texto--block">${dados.age} anos</span>
-          <span class="texto--block titulo--secundario">ONEhellper</span>
-        `;
-        document.querySelector('[data-info="trabalhos"]').innerHTML = `
-          <ul>
-            ${dados
-              .trabalhos
-              .map(({empresa, cargo, dataInicio, dataFinal}) => {
-              return `
-                <li class="texto">
-                  <span class="titulo--secundario">
-                    ${empresa} ( ${dataInicio} ${null? ' - . . .' :  ' - ' dataFinal} )
-                  </span> - ${cargo}
-                </li>
-              `})
-            }
-          </ul>
-        `;
-      });
-      
-
+  await fetch("./info.json", { method : "GET" })
+    .then((resposta) => resposta.json())
+    .then((dados) => {
+      console.log(dados);
+      document.querySelector('[data-info="eu"]').innerHTML = informacaoPessoal(dados);
+      document.querySelector('[data-info="trabalhos"]').innerHTML = `
+        <ul>
+          ${trabalhosRegistrados(dados)}
+        </ul>
+      `
+    })
 }
 
 mostraInformacaos();
-})()
-
+})();
