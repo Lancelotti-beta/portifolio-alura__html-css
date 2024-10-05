87f8b865cd5b9334ab3435c75a0bef60a3491ce2
Bruno José Marques Da Silva <78493454+Lancelotti-beta@users.noreply.github.com>
2024-10-05 20:30:49 +0000

    . . .

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, get } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
+//import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC9kLsETz37DBoTilkK5PjNQg_ldZTIWCc",
   authDomain: "bdtrabalhos.firebaseapp.com",
@@ -19,9 +15,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
-console.log(app);
+console.log(getDatabase());
 
 const terminoDeContrato = (value) => null ? " - . ." : " - " + value;
 
