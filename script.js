e38d50daa3a651e9fe0b377839078b8031045bac
Bruno José Marques Da Silva <78493454+Lancelotti-beta@users.noreply.github.com>
2024-10-06 05:01:20 +0000

    . .

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function informacaoPessoal(dados) {
 }
 
 function trabalhosRegistrados(dados) {
-  console.log(dados.trabalhos[0].dataInicio)
+  console.log(dados.trabalhos[0])
   dados.trabalhos.map(({ empresa, cargo, dataInicio, dataFinal }) => {
     return `
     <li class="texto">
