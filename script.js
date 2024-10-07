582d9ca5b893973b9a367c7c0594f52023e4a5a8
Bruno José Marques Da Silva <78493454+Lancelotti-beta@users.noreply.github.com>
2024-10-07 11:43:55 -0300

    Atualizando script.js

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
-//import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
+
 
 const firebaseConfig = {
-  //apiKey: "AIzaSyC9kLsETz37DBoTilkK5PjNQg_ldZTIWCc",
-  //authDomain: "bdtrabalhos.firebaseapp.com",
   databaseURL: "https://bdtrabalhos-default-rtdb.firebaseio.com",
-  //projectId: "bdtrabalhos",
-  //storageBucket: "bdtrabalhos.appspot.com",
-  //messagingSenderId: "85437939350",
-  //appId: "1:85437939350:web:ad14c4db28e7e521918cb7",
-  //measurementId: "G-L11VNXDR9V"
+  
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 
 const data = getDatabase(app);
 const referencia = ref(data)
